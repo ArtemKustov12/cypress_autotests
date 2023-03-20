dace012aad8a96b245216cfe6ab10b80a0462150
Artem Kustov <a.kustov@syberry.com>
2023-03-21 00:04:53 +0100

    add folder for the automationteststore website with 'contact-us' fole inside

---
+++ cypress-atomation-framework/cypress/e2e/automationteststore/contact-us.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+
+
+describe("Test Contack US form via WbdriverUni", () => {
+    it("Should be able to submit a successful submition via contact us form", () => {
+        // cypress code
+        cy.visit('https://automationteststore.com/');
+        cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.get('#ContactUsFrm_first_name').type('John');
+        cy.get('#ContactUsFrm_email').type('Smith@fake.xom');
+        cy.get('#ContactUsFrm_enquiry').type('test enquiry');
+        cy.get('.col-md-6 > .btn').click();
+    });
+
+    // it("Should NOT be able to submit a successful submition via contact us form as all fields are required", () => {
+    //     // cypress code
+    //     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html');
+    //     cy.get('[name="first_name"]').type('Joe');
+    //     cy.get('[name="last_name"]').type('Doe');
+    //     cy.get('textarea.feedback-input').type('Commets here');
+    //     cy.get('[type="submit"]').click();
+    // });
+});
