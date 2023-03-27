a2b99d5d816db92a60bca4d43762bce54611cea4
Artem Kustov <a.kustov@syberry.com>
2023-03-27 23:12:05 +0200

    add selectors.js file to the wedrivenuni, add assertions to the contact-us.js files

--- cypress-atomation-framework/cypress/e2e/automationteststore/contact-us.js
+++ cypress-atomation-framework/cypress/e2e/automationteststore/contact-us.js
@@ -7,11 +7,15 @@ describe("Test Contack US form via WbdriverUni", () => {
         // cypress code
         cy.visit('https://automationteststore.com/');
         //cy.get('.info_links_footer > :nth-child(5) > a').click();
-        cy.xpath("//a[contains(@href, 'contact')]").click();
+        //cy.xpath("//a[contains(@href, 'contact')]").click();
+        cy.get("a[href$='contact']").click();
         cy.get('#ContactUsFrm_first_name').type('John');
         cy.get('#ContactUsFrm_email').type('Smith@fake.xom');
+        cy.get('#ContactUsFrm_email').should('have.attr','name','email');
         cy.get('#ContactUsFrm_enquiry').type('test enquiry');
-        cy.get('.col-md-6 > .btn').click();
+        //cy.get('.col-md-6 > .btn').click();
+        cy.get("button[title='Submit']").click();
+        cy.get('.mb40 > :nth-child(3)').should('have.text','Your enquiry has been successfully sent to the store owner!');
     });
 
     // it("Should NOT be able to submit a successful submition via contact us form as all fields are required", () => {
--- cypress-atomation-framework/cypress/e2e/webdirveruniversity/contact-us.js
+++ cypress-atomation-framework/cypress/e2e/webdirveruniversity/contact-us.js
@@ -2,15 +2,19 @@
 
 
 describe("Test Contack US form via WbdriverUni", () => {
-    it("Should be able to submit a successful submition via contact us form", () => {
+    it.only("Should be able to submit a successful submition via contact us form", () => {
         // cypress code
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.document().should('have.property','charset').and('eq','UTF-8');
+        cy.title().should('include','WebDriver | Contact Us');
+        cy.url().should('include','contactus.html');
         // cy.get('#contact-us > .thumbnail').click();
         cy.get('[name="first_name"]').type('Joe');
         cy.get('[name="last_name"]').type('Doe');
         cy.get('[name="email"]').type('testemail@fake.com');
         cy.get('textarea.feedback-input').type('Commets here');
         cy.get('[type="submit"]').click();
+        cy.get('h1').should('have.text','Thank You for your Message!');
     });
 
     it("Should NOT be able to submit a successful submition via contact us form as all fields are required", () => {
@@ -20,5 +24,6 @@ describe("Test Contack US form via WbdriverUni", () => {
         cy.get('[name="last_name"]').type('Doe');
         cy.get('textarea.feedback-input').type('Commets here');
         cy.get('[type="submit"]').click();
+        cy.get('body').contains('Error: all fields are required');
     });
 });
