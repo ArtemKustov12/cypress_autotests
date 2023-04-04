e717748ffd0d82d1c7bbb39d1e519437ea248d33
Artem Kustov <a.kustov@syberry.com>
2023-04-04 23:31:55 +0200

    add new file alias-invoke.js

---
+++ cypress-atomation-framework/cypress/e2e/automationteststore/alias-invoke.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+/// <reference types="@cypress/xpath" />
+
+
+describe("Alias and Invoke", () => {
+    it('Validate product thumbnail', () => {
+        cy.visit('https://automationteststore.com/');
+
+        cy.get('.thumbnail').then(thumbnails => {
+            const thumbnailCount = thumbnails.length;
+            cy.log(`Found ${thumbnailCount} thumbnails on the page`);
+        }); 
+
+        // will not pass, there are no cart on each element
+        cy.get('.thumbnail')
+        .should('have.length', 16)
+        .each(($thumbnail) => {
+            cy.wrap($thumbnail).find('[title="Add to Cart"]').should('exist')
+        })
+    });
+
+});
