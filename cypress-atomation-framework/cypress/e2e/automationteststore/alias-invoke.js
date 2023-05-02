a942090fc3ae28b7903ce5e83b906f394486b3da
Artem Kustov <a.kustov@syberry.com>
2023-05-02 23:47:22 +0200

    within file alias-invoke Combining Alias , Invoke, Variables & Iterating through Data

--- cypress-atomation-framework/cypress/e2e/automationteststore/alias-invoke.js
+++ cypress-atomation-framework/cypress/e2e/automationteststore/alias-invoke.js
@@ -3,20 +3,26 @@
 
 
 describe("Alias and Invoke", () => {
-    it('Validate product thumbnail', () => {
+    // it('Validate product thumbnail', () => {
+    //     cy.visit('https://automationteststore.com/');
+
+    //     cy.get('.thumbnail').then(thumbnails => {
+    //         const thumbnailCount = thumbnails.length;
+    //         cy.log(`Found ${thumbnailCount} thumbnails on the page`);
+    //     }); 
+
+    //     // will not pass, there are no cart on each element
+    //     cy.get('.thumbnail')
+    //     .should('have.length', 16)
+    //     .each(($thumbnail) => {
+    //         cy.wrap($thumbnail).find('[title="Add to Cart"]').should('exist')
+    //     })
+    // });
+    
+    it('Calculate total of normal and sale products', () => {
         cy.visit('https://automationteststore.com/');
-
-        cy.get('.thumbnail').then(thumbnails => {
-            const thumbnailCount = thumbnails.length;
-            cy.log(`Found ${thumbnailCount} thumbnails on the page`);
-        }); 
-
-        // will not pass, there are no cart on each element
-        cy.get('.thumbnail')
-        .should('have.length', 16)
-        .each(($thumbnail) => {
-            cy.wrap($thumbnail).find('[title="Add to Cart"]').should('exist')
-        })
+        cy.get('.thumbnail').find('.oneprice').each(($el) => {
+            cy.log($el.text());
+          });        
     });
-
 });
